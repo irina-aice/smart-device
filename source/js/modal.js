545655ac580d7178a564d89be43ec90c9804b67a
irina-aice <61356164+irina-aice@users.noreply.github.com>
2021-07-21 14:52:03 +0300

    changes after programmer checking

--- source/js/modal.js
+++ source/js/modal.js
@@ -6,20 +6,31 @@
   const modalCloseButton = document.querySelector('.js-close-button');
   const modalInnerSelector = '.js-modal-inner';
   const form = modal.querySelector('.js-form');
+  const body = document.querySelector('body');
+  const documentWidth = document.documentElement.clientWidth;
+  const scrollbarWidth = Math.abs(window.innerWidth - documentWidth);
   let isOpen = false;
+
+  function closeModal() {
+    modal.setAttribute('data-state', 'close');
+    isOpen = false;
+    body.style.overflow = 'visible';
+    if (scrollbarWidth > 0) {
+      body.style.paddingRight = '';
+    }
+  }
 
   modalButton.addEventListener('click', (evt) => {
     evt.preventDefault();
     evt.stopPropagation();
     modal.setAttribute('data-state', 'open');
     isOpen = true;
-    form.querySelector('[name="fio"]').focus();
+    form.querySelector('.js-form-fio-input').focus();
+    body.style.overflow = 'hidden';
+    if (scrollbarWidth > 0) {
+      body.style.paddingRight = `${scrollbarWidth}px`;
+    }
   });
-
-  function closeModal() {
-    modal.setAttribute('data-state', 'close');
-    isOpen = false;
-  }
 
   modalCloseButton.addEventListener('click', (evt) => {
     evt.stopPropagation();
